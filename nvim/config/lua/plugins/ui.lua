5dd9d3b30d99af7fb975204e8c5d3952bafb679d
Theseus Grey <TheseusGrey@proton.me>
2024-02-25 14:45:20 +0000

    Added keymaps and config for focus mode

--- nvim/config/lua/plugins/ui.lua
+++ nvim/config/lua/plugins/ui.lua
@@ -1,7 +1,28 @@
+local function default_config_function(PluginSpec)
+  local options = nil
+
+  if type(PluginSpec.opts) == "function" then
+    options = PluginSpec.opts(PluginSpec)
+  elseif type(PluginSpec.opts) ~= "nil" then
+    options = PluginSpec.opts
+  end
+
+  require(PluginSpec.main).setup(options)
+end
+
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "junegunn/goyo.vim" },
+  {
+    "junegunn/goyo.vim",
+    opts = {
+      goyo_width = 120,
+    },
+    config = default_config_function,
+    keys = {
+      { "<leader>fo", "<cmd>Goyo<CR>", desc = "Toggle focus editing" },
+    },
+  },
   { "junegunn/limelight.vim" },
   --[=[{
     "Bekaboo/dropbar.nvim",
