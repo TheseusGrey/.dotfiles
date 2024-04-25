8e9eee4c306cc49006071f7d9dcfde5becab780a
Theseus Grey <TheseusGrey@proton.me>
2024-04-25 22:04:50 +0100

    QOL improvements to neotree filter and keymaps

--- nvim/config/lua/config/keymaps.lua
+++ nvim/config/lua/config/keymaps.lua
@@ -9,22 +9,3 @@ vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
 vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
 vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
 vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
-
-local neotree = require("neo-tree.command")
-vim.keymap.set("n", "<leader>e", function()
-  local reveal_file = vim.fn.expand("%:p")
-  if reveal_file == "" then
-    reveal_file = vim.fn.getcwd()
-  else
-    local f = io.open(reveal_file, "r")
-    if f then
-      f.close(f)
-    else
-      reveal_file = vim.fn.getcwd()
-    end
-  end
-  neotree.execute({
-    position = "float",
-    reveal_file = reveal_file,
-  })
-end)
--- nvim/config/lua/plugins/ui.lua
+++ nvim/config/lua/plugins/ui.lua
@@ -1,6 +1,33 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      { "<leader>E", false },
+      {
+        "<leader>e",
+        function()
+          local neotree = require("neo-tree.command")
+          local state = require("neo-tree.sources.manager").get_state("filesystem")
+          local reveal_file = vim.fn.expand("%:p")
+          if reveal_file == "" then
+            reveal_file = vim.fn.getcwd()
+          else
+            local f = io.open(reveal_file, "r")
+            if f then
+              f.close(f)
+            else
+              reveal_file = vim.fn.getcwd()
+            end
+          end
+          require("neo-tree.sources.filesystem.commands").clear_filter(state)
+          neotree.execute({
+            position = "float",
+            reveal_file = reveal_file,
+          })
+        end,
+        desc = "Neotree File explorer",
+      },
+    },
     event_handlers = {
       {
         event = "file_opened",
