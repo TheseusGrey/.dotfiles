97ab3c7f3328cc2ec402e156b934194312dbc117
Theseus Grey <TheseusGrey@proton.me>
2024-05-12 20:11:06 +0100

    Got the external editor script to work so I can use nvim to code godot stuff

--- scripts/godot.sh
+++ scripts/godot.sh
@@ -1,23 +1,7 @@
 #!/usr/bin/env bash
 
-# CHANGE IF NEEDED:
-# - replace with your Terminal Emulator executable
-term_exec="kitty"
-# - replace with your Neovim executable
-nvim_exec="nvim"
 # - replace with other path for the Neovim server pipe
 server_path="$HOME/.cache/nvim/godot-server.pipe"
+nvim_exec="nvim --listen $server_path $1"
 
-start_server() {
-	"$term_exec" -e "$nvim_exec" --listen "$server_path" "$1"
-}
-
-open_file_in_server() {
-	"$term_exec" -e "$nvim_exec" --server "$server_path" --remote-send "<C-\><C-n>:n $1<CR>:call cursor($2)<CR>"
-}
-
-if ! [ -e "$server_path" ]; then
-	start_server "$1"
-else
-	open_file_in_server "$1" "$2"
-fi
+tmux new-window -d "$nvim_exec"
