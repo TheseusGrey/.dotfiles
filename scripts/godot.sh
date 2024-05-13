60cdc4c2fe91e117e9d64fd71e98bf911cdae152
Theseus Grey <TheseusGrey@proton.me>
2024-05-13 09:14:58 +0100

    godot script now looks for named window in session, will pop open new tabs in the nvim window rather than new windows in the tmux session

--- scripts/godot.sh
+++ scripts/godot.sh
@@ -4,4 +4,10 @@
 server_path="$HOME/.cache/nvim/godot-server.pipe"
 nvim_exec="nvim --listen $server_path $1"
 
-tmux new-window -d "$nvim_exec"
+godot_window=$(tmux list-windows | grep 'godot' | awk 'BEGIN{FS=":|-"} {print$1}')
+
+if [ -n "$godot_window" ]; then
+	tmux send-keys -t 0:$godot_window.0 C-z ":e $1" Enter
+else
+	tmux new-window -n godot -d "$nvim_exec"
+fi
