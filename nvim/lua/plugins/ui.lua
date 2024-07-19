b52328e021c06f6ff860962e192142f63339721e
TheseusGrey <TheseusGrey@proton.me>
2024-07-19 16:04:28 +0100

    Let oil show hidden files (might wanna hide the `..` though)

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -83,6 +83,9 @@ return {
     "stevearc/oil.nvim",
     opts = {
       default_file_explorer = true,
+      view_options = {
+        show_hidden = true,
+      },
     },
     dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
