ddb5cc2849af237cdd7e800e7693d6fbf6a62b24
Paris Goldman-Smith <35925962+dent50cent@users.noreply.github.com>
2024-08-23 12:22:58 +0100

    Fix bufferline not loading properly, updated nvim deps

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,19 +1,18 @@
 return {
-	"akinsho/bufferline.nvim",
-	event = "VeryLazy",
-	version = "*",
-	dependencies = "nvim-tree/nvim-web-devicons",
-	config = function(_, opts)
-		local map = vim.keymap.set
+  "akinsho/bufferline.nvim",
+  version = "*",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function(_, opts)
+    local map = vim.keymap.set
 
-		map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
-		map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
-		map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
-		map("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
-		map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
-		map("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
-		map("n", "<leader>bd", "<cmd>bd<CR>", { desc = "Delete Buffer" })
-		map("n", "<leader>bD", "<cmd>:bd<cr>", { desc = "Delete Buffer and Window" })
-		require("bufferline").setup(opts)
-	end,
+    map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+    map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+    map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+    map("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+    map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+    map("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+    map("n", "<leader>bd", "<cmd>bd<CR>", { desc = "Delete Buffer" })
+    map("n", "<leader>bD", "<cmd>:bd<cr>", { desc = "Delete Buffer and Window" })
+    require("bufferline").setup(opts)
+  end,
 }
