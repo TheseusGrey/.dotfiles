234e812bfe19d0a373e9dfb5684278763996a7cf
TheseusGrey <TheseusGrey@proton.me>
2024-09-15 17:58:35 +0100

    Remove test for default file explorer stuff, and throw in minecraft because why not

--- configuration.nix
+++ configuration.nix
@@ -148,12 +148,12 @@
     cargo
     yazi
     kitty
+    minecraft
   ];
 
   environment.pathsToLink = [ "/share/zsh" ];
   programs.zsh.enable = true;
   programs.yazi.enable = true;
-  xdg.mimeApps.defaultApplications."inode/directory" = "dolphin.desktop";
 
   programs.steam.enable = true;
   hardware.opentabletdriver.enable = true;
