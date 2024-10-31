14acacd80498390dd6155a02d23bf3dd30e3541f
Paris Goldman-Smith <35925962+dent50cent@users.noreply.github.com>
2024-10-31 13:33:07 +0000

    Nvim updates and switch up the telescope display

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,16 +1,26 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-	},
-	config = function()
-		require("telescope").load_extension("fzf")
-	end,
-	keys = {
-		{ "<leader>ff", require("telescope.builtin").find_files, desc = "Find Files" },
-		{ "<leader><leader>", require("telescope.builtin").git_files, desc = "Find Git Files" },
-		{ "<leader>fg", require("telescope.builtin").live_grep, desc = "Grep files" },
-	},
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  },
+  config = function()
+    local t = require("telescope")
+
+    t.load_extension("fzf")
+    t.setup({
+      defaults = {
+        path_display = function(opts, path)
+          local tail = require("telescope.utils").path_tail(path)
+          return string.format("%s (%s)", tail, path)
+        end,
+      },
+    })
+  end,
+  keys = {
+    { "<leader>ff", require("telescope.builtin").find_files, desc = "Find Files" },
+    { "<leader><leader>", require("telescope.builtin").git_files, desc = "Find Git Files" },
+    { "<leader>fg", require("telescope.builtin").live_grep, desc = "Grep files" },
+  },
 }
