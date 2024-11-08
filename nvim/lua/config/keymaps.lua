049b1bf9f6cd178fe9114d27e18fa5dc9c0b214e
Theseus Grey <TheseusGrey@proton.me>
2024-11-08 01:05:54 +0000

    Lsp actions and pickers added for neovim, should make life easier again

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -26,3 +26,20 @@ map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
 map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
 map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
 map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
+
+-- lsp
+local source_action = function(_, action)
+  vim.lsp.buf.code_action({
+    apply = true,
+    context = {
+      only = { action },
+      diagnostics = {},
+    },
+  })
+end
+map({ "n", "v" }, "<leader>gD", vim.lsp.buf.declaration, { desc = "Goto Declaration" })
+map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+map({ "n", "v" }, "<leader>cA", source_action, { desc = "Source Action" })
+map({ "n", "v" }, "<leader>cl", vim.lsp.codelens.run, { desc = "Codelense" })
+map({ "n", "v" }, "<leader>cL", vim.lsp.codelens.refresh, { desc = "Codelense Refresh" })
+map({ "n", "v" }, "<leader>cr", vim.lsp.buf.rename, { desc = "Rename Symbol" })
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -22,5 +22,19 @@ return {
     { "<leader>ff", require("telescope.builtin").find_files, desc = "Find Files" },
     { "<leader><leader>", require("telescope.builtin").git_files, desc = "Find Git Files" },
     { "<leader>fg", require("telescope.builtin").live_grep, desc = "Grep files" },
+    { "<leader>gd", require("telescope.builtin").lsp_definitions, mode = { "n", "v" }, desc = "Goto Definition" },
+    { "<leader>gr", require("telescope.builtin").lsp_references, mode = { "n", "v" }, desc = "Goto references" },
+    {
+      "<leader>gI",
+      require("telescope.builtin").lsp_implementations,
+      mode = { "n", "v" },
+      desc = "Goto Implementation",
+    },
+    {
+      "<leader>gy",
+      require("telescope.builtin").lsp_type_definitions,
+      mode = { "n", "v" },
+      desc = "Goto Type Definition",
+    },
   },
 }
