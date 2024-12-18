6500471e6e3db87b24afcf19576dae3e96f26476
Paris Goldman-Smith <35925962+dent50cent@users.noreply.github.com>
2024-12-18 14:50:55 +0000

    Fixed issues with new dashboard, added some git info too

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,10 +1,14 @@
 return {
   "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
   opts = {
     gitbrowse = { enabled = true },
     input = { enabled = true },
     lazygit = { enabled = true },
+    git = { enabled = true },
     quickfile = { enabled = true },
+    notifier = { enabled = true },
     dim = { enabled = true },
     statuscolumn = { enabled = true },
     zen = { enabled = true },
@@ -14,6 +18,19 @@ return {
         { section = "header" },
         { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
         { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        {
+          icon = " ",
+          title = "Git Status",
+          section = "terminal",
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = "git status --short --branch --renames",
+          height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+        },
         { section = "startup" },
       },
     },
@@ -22,9 +39,8 @@ return {
     {
       "<leader>fo",
       function()
-        local s = require("snacks")
-        s.zen()
-        s.dim()
+        Snacks.zen()
+        Snacks.dim()
       end,
       desc = "Toggle focus editing",
     },
