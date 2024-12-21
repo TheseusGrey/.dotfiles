63c46d4f388c396aa8671e964b3e820dd90da227
Theseus Grey <TheseusGrey@proton.me>
2024-12-21 18:59:58 +0000

    Script for single window focus works, just need to add it to run as part of the hypr config

--- bin/center_window.sh
+++ bin/center_window.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
+last_count=-1
 # Infinite loop to check the current workspace
 while true; do
   # Workspace info
   workspace=$(hyprctl activeworkspace)
-  active_workspace=$($workspace | grep "workspace ID" | awk '{print $3}')
-  window_count=$($workspace | grep "windows" | awk '{print $2}')
+  active_workspace=$(echo "$workspace" | grep "workspace ID" | awk '{print $3}')
+  window_count=$(echo "$workspace" | grep "windows" | awk '{print $2}')
 
-  # If there is exactly one window in the workspace
-  if [ "$window_count" -eq 1 ]; then
-    hyprctl dispatch setfloating
-    hyprctl dispatch resizeactive exact 80% 80%
-    hyprctl dispatch centerwindow
-  else
-    # Make sure that there are no floating windows, currently focus shifts to the new window, need it to not
-    hyprctl dispatch settiled
+  if [ "$window_count" -ne "$last_count" ]; then
+    # If there is exactly one window in the workspace
+    if [ "$window_count" -eq 1 ]; then
+      hyprctl dispatch setfloating
+      hyprctl dispatch resizeactive exact 80% 80%
+      hyprctl dispatch centerwindow
+    else
+      for i in {1..$window_count}; do
+        hyprctl dispatch cyclenext
+        hyprctl dispatch settiled
+      done
+    fi
+    last_count="$window_count"
   fi
 
   # Sleep to avoid busy-waiting (adjust as needed)
