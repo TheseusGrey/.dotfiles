d2bed9676ccd4c5297e1f0e6a2a948410a8332b3
Theseus Grey <TheseusGrey@proton.me>
2025-01-05 00:05:31 +0000

    nvim package updates, and switching to markview for present styling for now

--- nvim/lua/plugins/present.lua
+++ nvim/lua/plugins/present.lua
@@ -1,3 +1,23 @@
 return {
-  dir = "~/projects/present.nvim",
+  {
+    dir = "~/projects/present.nvim",
+    opts = {
+      integrations = {
+        markview = true,
+      },
+    },
+  },
+  { -- for pretty markdown styling
+    "OXY2DEV/markview.nvim",
+    lazy = true,
+    ft = "markdown",
+    opts = {
+      initial_state = false,
+    },
+
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
 }
