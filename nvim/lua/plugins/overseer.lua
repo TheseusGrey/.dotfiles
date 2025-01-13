f7fa6c1808fd338d0bd371988dcb125bd7b7260e
Paris Goldman-Smith <35925962+dent50cent@users.noreply.github.com>
2025-01-13 15:12:50 +0000

    overseer keybinds

--- nvim/lua/plugins/overseer.lua
+++ nvim/lua/plugins/overseer.lua
@@ -5,4 +5,8 @@ return {
       direction = "right",
     },
   },
+  keys = {
+    { "<leader>or", "<cmd>OverseerRun<cr>", desc = "Run a task through overseer" },
+    { "<leader>ol", "<cmd>OverseerToggle<cr>", desc = "Toggle Overseer task list" },
+  },
 }
