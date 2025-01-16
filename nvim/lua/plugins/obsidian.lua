cfdbc44df718182c7aa88190c0be1fbe3a46a45a
Theseus Grey <TheseusGrey@proton.me>
2025-01-16 00:57:55 +0000

    Testing and bug fixing obsidian-bridge

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -31,4 +31,19 @@ return {
       },
     },
   },
+  {
+    "oflisback/obsidian-bridge.nvim",
+    opts = {
+      obsidian_server_address = "https://127.0.0.1:27124",
+      cert_path = "~/Documents/obsidian.crt",
+    },
+    event = {
+      "BufReadPre " .. vault_location .. "/**.md",
+      "BufNewFile " .. vault_location .. "/**.md",
+    },
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
 }
