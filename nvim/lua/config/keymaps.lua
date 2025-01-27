5f886425d512a76270913d3f68d6232a371754fe
Paris Goldman-Smith <35925962+dent50cent@users.noreply.github.com>
2025-01-27 18:12:32 +0000

    nvim navigation keymaps and packages updates

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,6 +3,12 @@ local map = vim.keymap.set
 -- Clear search with <esc>
 map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
 
+-- Move to window using the <ctrl> hjkl keys
+map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
 -- Don't yank empty lines
 vim.keymap.set("n", "dd", function()
   if vim.fn.getline("."):match("^%s*$") then
