9421f5386d43c540ae144d70996eff2521ca7da2
Paris Goldman-Smith <35925962+dent50cent@users.noreply.github.com>
2025-03-04 13:25:06 +0000

    nvim doing fix keymap descriptions

--- nvim/lua/plugins/doing.lua
+++ nvim/lua/plugins/doing.lua
@@ -10,21 +10,21 @@ return {
       function()
         require("doing").add()
       end,
-      { desc = "Add task to TODO list" },
+      desc = "Add task to TODO list",
     },
     {
       "<leader>dd",
       function()
         require("doing").done()
       end,
-      { desc = "Mark a task as done" },
+      desc = "Mark a task as done",
     },
     {
       "<leader>de",
       function()
         require("doing").edit()
       end,
-      { desc = "Edit the task list" },
+      desc = "Edit the task list",
     },
   },
 }
