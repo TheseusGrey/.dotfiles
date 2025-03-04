56a1473e1ef6a4eeebda688ffb9fa70334f82805
Paris Goldman-Smith <35925962+dent50cent@users.noreply.github.com>
2025-03-04 11:38:00 +0000

    nvim cmp trim down keymaps, moving away from tab

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -92,8 +92,8 @@ return {
           { name = "obsidian.nvim" },
         },
         mapping = {
-          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-          ["<Tab>"] = cmp.mapping.select_next_item(),
+          -- ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          -- ["<Tab>"] = cmp.mapping.select_next_item(),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
