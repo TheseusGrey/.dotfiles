cc7ff8d1f1634c82fb6ef1a850421ee9d405627e
Paris Goldman-Smith <35925962+dent50cent@users.noreply.github.com>
2025-04-15 13:07:36 +0100

    nvim remove carpenter until I have time to work on it some more

--- nvim/lua/plugins/carpenter.lua
+++ nvim/lua/plugins/carpenter.lua
@@ -1,12 +1,13 @@
-return {
-  "TheseusGrey/carpenter.nvim",
-  dev = true,
-  dir = "~/projects/carpenter.nvim",
-  event = { "VeryLazy" },
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-lua/plenary.nvim",
-  },
-  ft = "markdown",
-  opts = {},
-}
+return {}
+-- return {
+--   "TheseusGrey/carpenter.nvim",
+--   dev = true,
+--   dir = "~/projects/carpenter.nvim",
+--   event = { "VeryLazy" },
+--   dependencies = {
+--     "nvim-treesitter/nvim-treesitter",
+--     "nvim-lua/plenary.nvim",
+--   },
+--   ft = "markdown",
+--   opts = {},
+-- }
