d7597386f4b002e4b429ca6da7c53abd0b3084b8
Paris Goldman-Smith <p.goldmansmith1@ocado.com>
2025-10-17 14:26:42 +0100

    nvim updated conform to use prettier for css (cssls had ugly formatting)

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,42 +1,44 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function(args)
-		require("conform").format({ bufnr = args.buf })
-	end,
+  pattern = "*",
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf })
+  end,
 })
 
 return {
-	"stevearc/conform.nvim",
-	dependencies = { "mason.nvim" },
-	lazy = true,
-	cmd = "ConformInfo",
-	keys = {
-		{
-			"<leader>cF",
-			function()
-				require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
-			end,
-			mode = { "n", "v" },
-			desc = "Format Injected Langs",
-		},
-	},
-	opts = {
-		default_format_opts = {
-			timeout_ms = 3000,
-			async = false, -- not recommended to change
-			quiet = false, -- not recommended to change
-			lsp_format = "fallback", -- not recommended to change
-		},
-		formatters_by_ft = {
-			lua = { "stylua" },
-			fish = { "fish_indent" },
-			sh = { "shfmt" },
-			python = { "isort", "black" },
-			rust = { "rustfmt", lsp_format = "fallback" },
-			javascript = { "prettierd", "prettier", stop_after_first = true },
-		},
-		formatters = {
-			injected = { options = { ignore_errors = true } },
-		},
-	},
+  "stevearc/conform.nvim",
+  dependencies = { "mason.nvim" },
+  lazy = true,
+  cmd = "ConformInfo",
+  keys = {
+    {
+      "<leader>cF",
+      function()
+        require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+      end,
+      mode = { "n", "v" },
+      desc = "Format Injected Langs",
+    },
+  },
+  opts = {
+    default_format_opts = {
+      timeout_ms = 3000,
+      async = false, -- not recommended to change
+      quiet = false, -- not recommended to change
+      lsp_format = "fallback", -- not recommended to change
+    },
+    formatters_by_ft = {
+      lua = { "stylua" },
+      fish = { "fish_indent" },
+      sh = { "shfmt" },
+      python = { "isort", "black" },
+      rust = { "rustfmt", lsp_format = "fallback" },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+      css = { "prettier", "cssls", stop_after_first = true },
+      scss = { "prettier", "cssls", stop_after_first = true },
+    },
+    formatters = {
+      injected = { options = { ignore_errors = true } },
+    },
+  },
 }
