9938c9f7f62aeb203137b2b52593fdd445950eb1
Theseus Grey <TheseusGrey@proton.me>
2026-03-21 23:02:49 +0000

    dots putting some comments in that I can turn into a stuff later

--- setup.sh
+++ setup.sh
@@ -15,6 +15,17 @@ link() {
   stow -v -R -t ${t} ${s}
 }
 
+# /usr/share/applications
+# Want to create some desktop files for tools I use
+# yazi
+# wikiman
+# tldr
+#
+# An example for yazi already exists, we just need to move it here and symlink it properly
+# We'll create an applications folder and link it to the path above
+
+# Also, I want to include package lists to install a baseline incase I need it in future.
+
 configs=(
   zsh
   nvim
